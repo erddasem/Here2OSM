a95b5df44d8d6c8a048ffdb8ca5e56df366bf89f
Emily <erddasemmy@gmail.com>
2020-03-17 12:15:35 +0100

    Decoder + spatial queries

--- src/main/java/HereApi/XMLParser.java
+++ src/main/java/HereApi/XMLParser.java
@@ -6,6 +6,7 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
 import org.w3c.dom.*;
 import javax.xml.parsers.*;
