f981705d249671ed0216bf73b38f5d33844d29ec
Emily <erddasemmy@gmail.com>
2020-11-04 14:59:55 +0100

    Finished code

--- src/main/java/GeometryFunctions/GeometryFunctions.java
+++ src/main/java/GeometryFunctions/GeometryFunctions.java
@@ -13,44 +13,24 @@ import java.util.List;
 
 public class GeometryFunctions {
 
-    private static Geometry createSearchArea(double latitude, double longitude, double distance) {
-        GeometricShapeFactory shapeFactory = new GeometricShapeFactory();
-        shapeFactory.setNumPoints(32);
-        shapeFactory.setCentre(new Coordinate(latitude, latitude));
-        shapeFactory.setSize(distance * 2);
-        return shapeFactory.createCircle();
-    }
-
-    public static List<NodeImpl> getNodesWithinDistance(List<NodeImpl> nodes, double latitude, double longitude, double distance) {
-        Polygon area = createSearchArea(latitude, longitude, distance).getFactory().createPolygon();
-        List<NodeImpl> nodesWithinDistance = new ArrayList<>();
-        nodes.forEach(n -> {
-            if (n.getPointGeometry().within(area)) {
-                nodesWithinDistance.add(n);
-            }
-        });
-
-        return nodesWithinDistance;
-    }
-
-
-    public static List<LineImpl> getLinesWithinDistance(List<LineImpl> lines, double latitude, double longitude, double distance) {
-        Polygon area = createSearchArea(latitude, longitude, distance).getFactory().createPolygon();
-        List<LineImpl> linesWithinDistance = new ArrayList<>();
-        lines.forEach(l -> {
-            if (l.getLineGeometry().within(area)) {
-                linesWithinDistance.add(l);
-            }
-        });
-
-        return linesWithinDistance;
-    }
-
+    /**
+     * Converts meter to degree.
+     * @param lat latitude
+     * @param dist distance in meter
+     * @return returns distance in degree
+     */
     public static double distToDeg(double lat, double dist) {
+
         return dist / (111.32 * 1000 * Math.cos(lat * (Math.PI / 180)));
     }
 
+    /**
+     * Converts degree to meter.
+     * @param distDeg distance in degree
+     * @return distance in meter
+     */
     public static int distToMeter(double distDeg) {
+
         return (int) Math.round(distDeg * (Math.PI/180) * 6378137);
     }
 
